09b3622fa19ce1ec7762abfb3d6aedd023eeb758
Mikołaj Klarżuk <mickey@megarush.com>
2021-08-26 16:28:13 +0200

    Project preparations

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="nav">
+    <img class="nav__logo" alt="Vue logo" src="@/assets/logo.png">
+    <div class="nav__links">
+      <router-link class="nav__link" to="/">Home</router-link>
+      <router-link class="nav__link" to="/basics">Basics</router-link>
+    </div>
+    </div>
+  <div class="content">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
+
+body {
+  margin: 0;padding: 0;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+#app {
+  height: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $madison;
+
+  display: grid;
+  grid-template-columns: 150px 1fr;
+
 }
 
-#nav {
-  padding: 30px;
+.nav {
+  .nav__logo {
+    height: 100px;
+    width: 100px;
+  }
+  .nav__links {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    .nav__link {
+      font-weight: bold;
+      color: $madison;
+        &.router-link-exact-active {
+          color: $medium-sea-green;
+        }
     }
   }
 }
+
+.content {
+  text-align: center;
+  border: 1px solid $medium-sea-green;
+}
+
 </style>
